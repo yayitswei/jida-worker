682e9170490aa371c8c9a715aed71642f6a8a15c
Sean Grove <s@bushi.do>
2012-11-18 12:35:58 -0800

    Add fs for filesystem utils

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject jida-worker "0.1.0-SNAPSHOT"
+  :plugins [[lein-swank "1.4.0"]]
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-redis "0.0.12"]]
+                 [clj-redis "0.0.12"]
+                 [fs/fs "1.3.2"]]
   :main jida-worker.core)
