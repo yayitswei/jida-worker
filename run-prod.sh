ed5a5f36164b5547c9e9f0b94c485bb22f1d50ab
Wei Hsu <wei@airtime.com>
2012-11-18 11:27:40 -0800

    change redis uri, def config vars above

--- run-prod.sh
+++ run-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-REDISTOGO_URL=redis://redistogo:dfcab96405c0df37b9e0088d69995010@gar.redistogo.com:9184/ CODEQ_JAR_PATH=/usr/app/jikken/repos/codeq/target/codeq-0.1.0-SNAPSHOT-standalone.jar DATOMIC_URI=datomic:free://96.126.103.193:4334/git java -jar jida-worker-0.1.0-SNAPSHOT-standalone.jar
+REDIS_URI=redis://96.126.103.193:6379/ CODEQ_JAR_PATH=/usr/app/jikken/repos/codeq/target/codeq-0.1.0-SNAPSHOT-standalone.jar DATOMIC_URI=datomic:free://96.126.103.193:4334/git java -jar jida-worker-0.1.0-SNAPSHOT-standalone.jar
--- src/jida_worker/core.clj
+++ src/jida_worker/core.clj
@@ -3,14 +3,22 @@
   (:require [clj-redis.client :as redis])
   (:gen-class))
 
-(def default-redis-uri "redis://redistogo:dfcab96405c0df37b9e0088d69995010@gar.redistogo.com:9184/")
-(def default-codeq-jar-path"/Users/wei/projects/clojure/codeq/target/codeq-0.1.0-SNAPSHOT-standalone.jar")
-(def default-datomic-uri "datomic:free://localhost:4334/git")
-(def redis-conn (atom nil))
+(def codec-jar-path
+  (or (System/getenv "CODEQ_JAR_PATH")
+      "/Users/wei/projects/clojure/codeq/target/codeq-0.1.0-SNAPSHOT-standalone.jar"))
 
+(def datomic-uri
+  (or (System/getenv "DATOMIC_URI")
+      "datomic:free://localhost:4334/git"))
+
+(def redis-uri
+  (or (System/getenv "REDIS_URI")
+      "redis://localhost"))
+
+(def redis-conn (atom nil))
 (defn db []
   (or @redis-conn
-      (let [uri (or (System/getenv "REDISTOGO_URL") default-redis-uri)]
+      (let [uri redis-uri]
         (reset! redis-conn (redis/init :url uri)))))
 
 (def working-dir "/tmp")
@@ -23,10 +31,11 @@
     (with-sh-dir
       (str working-dir "/" dirname)
       (println "Importing into codeq..")
-      (println (sh "java"
-          "-server" "-Xmx1g"
-          "-jar" (or (System/getenv "CODEQ_JAR_PATH") default-codeq-jar-path)
-          (or (System/getenv "DATOMIC_URI") default-datomic-uri)))
+      (println
+        (sh "java"
+            "-server" "-Xmx1g"
+            "-jar" codec-jar-path
+            datomic-uri))
       (println "Done."))))
 
 (defn get-tasks []
