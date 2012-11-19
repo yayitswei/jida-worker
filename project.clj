a3d693bec12ec5fbbb8547bafe456f447acd5b4d
Sean Grove <s@bushi.do>
2012-11-18 17:47:54 -0800

    Bring in codeq as a dependency so we can use it programmatically. Unfortunately, using it as a lib means we have to use clojure 1.5.0 (not sure why, but 1.4.0 causes some bad problems). Unfortunately, using clojure 1.5.0 means that swank-clojure is broken, and has been since clojure 1.5.0-alpha3. There goes half a day of fiddling.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-beta1"]
+                 [org.cloudhoist/codeq "0.1.0-SNAPSHOT"]
                  [clj-redis "0.0.12"]
                  [fs/fs "1.3.2"]]
   :main jida-worker.core)
